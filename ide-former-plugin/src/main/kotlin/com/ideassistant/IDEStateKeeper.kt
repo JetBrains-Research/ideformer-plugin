b82c1d8e6d463f64d0c04c2f81d8cb7027fad078
Evgeniia.Fedotova <ginger.ll@mail.ru>
2023-09-07 16:05:08 +0200

    add undo last api call method to IDE state keeper

--- ide-former-plugin/src/main/kotlin/com/ideassistant/IDEStateKeeper.kt
+++ ide-former-plugin/src/main/kotlin/com/ideassistant/IDEStateKeeper.kt
@@ -3,12 +3,22 @@ package com.ideassistant
 import java.util.*
 
 class IDEStateKeeper {
-    private val apiCallStack: Stack<IDEApiMethod> = Stack<IDEApiMethod>()
+    private val apiCallStack: Stack<Reversable> = Stack<Reversable>()
 
     fun saveApiCall(apiCall: IDEApiMethod) {
         if (apiCall is Reversable) {
             apiCallStack.push(apiCall)
         }
+    }
+
+    fun undoLastApiCall() {
+        if (apiCallStack.empty()) {
+            return
+        }
+
+        val lastApiCall = apiCallStack.peek()
+        lastApiCall.reverse()
+        apiCallStack.pop()
     }
 }
 
