925ba83660101a4dfee3a9fee4a154222b2d8b0a
Evgeniia.Fedotova <ginger.ll@mail.ru>
2023-10-17 14:53:14 +0200

    set group and version in build.gradle.kts files

--- ide-former-plugin/ide-server/build.gradle.kts
+++ ide-former-plugin/ide-server/build.gradle.kts
@@ -1,3 +1,6 @@
+group = rootProject.group
+version = rootProject.version
+
 dependencies {
     implementation("io.ktor:ktor-server-core:2.3.3")
     implementation("io.ktor:ktor-server-netty:2.3.3")
--- ide-former-plugin/plugin/build.gradle.kts
+++ ide-former-plugin/plugin/build.gradle.kts
@@ -1,3 +1,6 @@
+group = rootProject.group
+version = rootProject.version
+
 dependencies {
     implementation(project(":ide-core"))
     implementation(project(":ide-server"))
