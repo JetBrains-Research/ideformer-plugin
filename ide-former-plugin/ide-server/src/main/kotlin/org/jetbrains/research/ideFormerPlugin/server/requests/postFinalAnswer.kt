05531c366f54113db25a0b4096785542b019ff62
Evgeniia.Fedotova <ginger.ll@mail.ru>
2023-10-25 20:18:05 +0200

    add execute exception handling in the save final answer server request

--- ide-former-plugin/ide-server/src/main/kotlin/org/jetbrains/research/ideFormerPlugin/server/requests/postFinalAnswer.kt
+++ ide-former-plugin/ide-server/src/main/kotlin/org/jetbrains/research/ideFormerPlugin/server/requests/postFinalAnswer.kt
@@ -14,15 +14,16 @@ fun Routing.postFinalAnswer(logger: Logger, ideStateKeeper: IdeStateKeeper) {
         logger.info("Server POST final model answer request is called")
         val modelFinalAns = call.receiveText()
 
-        val apiMethod = SaveModelFinalAns(modelFinalAns)
+        val saveModelFinalAns = SaveModelFinalAns(modelFinalAns)
         try {
-            apiMethod.execute()
+            saveModelFinalAns.execute()
         } catch (e: Exception) {
             logger.error("Error while saving final answer api execution: ${e.message}")
+            return@post call.respondText ( e.message ?: "Error while save model final ans api execution: ${e.message}")
         }
         logger.info("Save model final ans api call was executed")
 
-        ideStateKeeper.saveReversibleApiMethod(apiMethod)
+        ideStateKeeper.saveReversibleApiMethod(saveModelFinalAns)
         logger.info("Save model final ans api call was added to the api methods stack")
 
         call.respondText(jsonConverter.toJson("Model ans was successfully saved"))
