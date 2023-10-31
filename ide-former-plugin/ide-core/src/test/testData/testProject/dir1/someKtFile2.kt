e604f13603a60e39c23567697a1eeb0965e6814f
Evgeniia.Fedotova <ginger.ll@mail.ru>
2023-10-31 22:04:30 +0100

    update someKtFile2.kt test example, update tests

--- ide-former-plugin/ide-core/src/test/testData/testProject/dir1/someKtFile2.kt
+++ ide-former-plugin/ide-core/src/test/testData/testProject/dir1/someKtFile2.kt
@@ -1,5 +1,12 @@
-class SomeClass(private val num: Int = 0) {
+class SimpleClass(private val num: Int = 0) {
     fun decreaseNum(): Int = return ++num
 
-    fun printSomePhrase() = println("some phrase")
+    fun printSomePhrase(phrase: String = "some phrase") = println(phrase)
+}
+
+class ComplexClass(
+    val prettyPhrase: String = "This is a pretty phrase",
+    private val simpleClass: SimpleClass = SimpleClass(num = 2)
+) {
+    fun delegatePrinting() = simpleClass.printSomePhrase(prettyPhrase)
 }
