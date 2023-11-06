b9acd1f4d3ef6711571e83b7edc364dd0eb786b1
Evgeniia.Fedotova <ginger.ll@mail.ru>
2023-11-06 23:06:29 +0100

    add choosing file api util to server file related requests

--- ide-former-plugin/ide-server/src/main/kotlin/org/jetbrains/research/ideFormerPlugin/server/requests/fileRelated/getFileClasses.kt
+++ ide-former-plugin/ide-server/src/main/kotlin/org/jetbrains/research/ideFormerPlugin/server/requests/fileRelated/getFileClasses.kt
@@ -1,12 +1,9 @@
 package org.jetbrains.research.ideFormerPlugin.server.requests.fileRelated
 
-import com.intellij.util.PathUtil
 import io.ktor.http.*
 import io.ktor.server.application.*
 import io.ktor.server.routing.*
-import org.jetbrains.research.ideFormerPlugin.api.models.fileRelated.fileClasses.JavaFileClasses
-import org.jetbrains.research.ideFormerPlugin.api.models.fileRelated.fileClasses.KtFileClasses
-import org.jetbrains.research.ideFormerPlugin.api.models.fileRelated.fileClasses.PyFileClasses
+import org.jetbrains.research.ideFormerPlugin.api.models.utils.chooseFileClassesApiForFile
 import org.jetbrains.research.ideFormerPlugin.server.IdeServerConstants
 import org.jetbrains.research.ideFormerPlugin.server.executeAndRespondError
 import org.jetbrains.research.ideFormerPlugin.server.respondJson
@@ -25,17 +22,10 @@ fun Routing.getFileClasses(logger: Logger, ideStateKeeper: IdeStateKeeper) {
         }
         logger.info("Server GET file classes request for file '$fileName' is called")
 
-        val fileExtension = PathUtil.getFileExtension(fileName)
-        val fileClasses = when (fileExtension) {
-            "kt" -> KtFileClasses(ideStateKeeper.currentProjectDirectory, fileName)
-            "java" -> JavaFileClasses(ideStateKeeper.currentProjectDirectory, fileName)
-            "py" -> PyFileClasses(ideStateKeeper.currentProjectDirectory, fileName)
-            else -> null
-        }
-
-        // TODO: extract to the separate function
-        if (fileClasses == null) {
-            logger.error("Incorrect request file extension: $fileExtension")
+        val fileClasses = try {
+            chooseFileClassesApiForFile(fileName, ideStateKeeper.currentProjectDirectory)
+        } catch (e: Exception) {
+            logger.error(e.message)
             return@get call.respondJson(
                 IdeServerConstants.INCORRECT_REQUESTED_FILE_EXTENSION,
                 HttpStatusCode.BadRequest
--- ide-former-plugin/ide-server/src/main/kotlin/org/jetbrains/research/ideFormerPlugin/server/requests/fileRelated/getFileFunctions.kt
+++ ide-former-plugin/ide-server/src/main/kotlin/org/jetbrains/research/ideFormerPlugin/server/requests/fileRelated/getFileFunctions.kt
@@ -1,12 +1,9 @@
 package org.jetbrains.research.ideFormerPlugin.server.requests.fileRelated
 
-import com.intellij.util.PathUtil.getFileExtension
 import io.ktor.http.*
 import io.ktor.server.application.*
 import io.ktor.server.routing.*
-import org.jetbrains.research.ideFormerPlugin.api.models.fileRelated.fileFunctions.JavaFileFunctions
-import org.jetbrains.research.ideFormerPlugin.api.models.fileRelated.fileFunctions.KtFileFunctions
-import org.jetbrains.research.ideFormerPlugin.api.models.fileRelated.fileFunctions.PyFileFunctions
+import org.jetbrains.research.ideFormerPlugin.api.models.utils.chooseFileFunctionsApiForFile
 import org.jetbrains.research.ideFormerPlugin.server.IdeServerConstants
 import org.jetbrains.research.ideFormerPlugin.server.executeAndRespondError
 import org.jetbrains.research.ideFormerPlugin.server.respondJson
@@ -25,17 +22,10 @@ fun Routing.getFileFunctions(logger: Logger, ideStateKeeper: IdeStateKeeper) {
         }
         logger.info("Server GET file methods request for file '$fileName' is called")
 
-        val fileExtension = getFileExtension(fileName)
-        val fileFunctions = when (fileExtension) {
-            "kt" -> KtFileFunctions(ideStateKeeper.currentProjectDirectory, fileName)
-            "java"  -> JavaFileFunctions(ideStateKeeper.currentProjectDirectory, fileName)
-            "py" -> PyFileFunctions(ideStateKeeper.currentProjectDirectory, fileName)
-            else -> null
-        }
-
-        // TODO: extract to the separate function
-        if (fileFunctions == null) {
-            logger.error("Incorrect request file extension: $fileExtension")
+        val fileFunctions = try {
+            chooseFileFunctionsApiForFile(fileName, ideStateKeeper.currentProjectDirectory)
+        } catch (e: Exception) {
+            logger.error(e.message)
             return@get call.respondJson(
                 IdeServerConstants.INCORRECT_REQUESTED_FILE_EXTENSION,
                 HttpStatusCode.BadRequest
