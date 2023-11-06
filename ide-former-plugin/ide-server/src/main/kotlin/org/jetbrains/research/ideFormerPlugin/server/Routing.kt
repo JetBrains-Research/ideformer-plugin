418a732be418df58368c34de14918f084c397508
Evgeniia.Fedotova <ginger.ll@mail.ru>
2023-11-06 23:17:46 +0100

    move routing utils to the separate file

--- ide-former-plugin/ide-server/src/main/kotlin/org/jetbrains/research/ideFormerPlugin/server/Routing.kt
+++ ide-former-plugin/ide-server/src/main/kotlin/org/jetbrains/research/ideFormerPlugin/server/Routing.kt
@@ -1,12 +1,7 @@
 package org.jetbrains.research.ideFormerPlugin.server
 
-import com.google.gson.Gson
-import io.ktor.http.*
 import io.ktor.server.application.*
-import io.ktor.server.response.*
 import io.ktor.server.routing.*
-import io.ktor.util.pipeline.*
-import org.jetbrains.research.ideFormerPlugin.api.models.IdeApiMethod
 import org.jetbrains.research.ideFormerPlugin.server.requests.*
 import org.jetbrains.research.ideFormerPlugin.server.requests.fileRelated.getFileClasses
 import org.jetbrains.research.ideFormerPlugin.server.requests.fileRelated.getFileFunctions
@@ -31,41 +26,6 @@ fun Application.configureRouting(ideStateKeeper: IdeStateKeeper, logger: Logger)
         getReverseApiMethods(logger, ideStateKeeper)
         postFinalAnswer(logger, ideStateKeeper)
     }
-}
-
-object RoutingUtils {
-    val jsonConverter = Gson()
-}
-
-suspend fun ApplicationCall.respondJson(responseObject: Any, status: HttpStatusCode = HttpStatusCode.OK) {
-    this.respondText(
-        text = RoutingUtils.jsonConverter.toJson(responseObject),
-        status = status
-    )
-}
-
-suspend fun ApplicationCall.processFileNameParameter(logger: Logger): String? =
-    this.parameters["fileName"] ?: run {
-        logger.error("File name was not provided")
-        this.respondJson(
-            IdeServerConstants.MISSING_FILENAME,
-            HttpStatusCode.BadRequest
-        )
-        null
-    }
-
-suspend fun PipelineContext<Unit, ApplicationCall>.executeAndRespondError(
-    ideApiMethod: IdeApiMethod,
-    logger: Logger
-): Boolean = try {
-    ideApiMethod.execute()
-    true
-} catch (e: Exception) {
-    logger.error("Error while api execution: ${e.message}")
-    call.respondJson(
-        e.message ?: IdeServerConstants.API_EXECUTION_UNKNOWN_ERROR
-    )
-    false
 }
 
 object IdeServerConstants {
