dc908dc1d3a4032012b2ebee4c8407ca8f271685
Evgeniia.Fedotova <ginger.ll@mail.ru>
2023-11-10 16:48:11 +0100

    move file extensions constants to the enum class

--- ide-former-plugin/ide-core/src/main/kotlin/org/jetbrains/research/ideFormerPlugin/api/models/utils/ApiUtils.kt
+++ ide-former-plugin/ide-core/src/main/kotlin/org/jetbrains/research/ideFormerPlugin/api/models/utils/ApiUtils.kt
@@ -2,30 +2,30 @@ package org.jetbrains.research.ideFormerPlugin.api.models.utils
 
 import com.intellij.psi.PsiDirectory
 import com.intellij.util.PathUtil
-import org.jetbrains.research.ideFormerPlugin.api.models.fileRelated.fileClasses.FileClasses
-import org.jetbrains.research.ideFormerPlugin.api.models.fileRelated.fileClasses.JavaFileClasses
-import org.jetbrains.research.ideFormerPlugin.api.models.fileRelated.fileClasses.KtFileClasses
-import org.jetbrains.research.ideFormerPlugin.api.models.fileRelated.fileClasses.PyFileClasses
-import org.jetbrains.research.ideFormerPlugin.api.models.fileRelated.fileFunctions.FileFunctions
-import org.jetbrains.research.ideFormerPlugin.api.models.fileRelated.fileFunctions.JavaFileFunctions
-import org.jetbrains.research.ideFormerPlugin.api.models.fileRelated.fileFunctions.KtFileFunctions
-import org.jetbrains.research.ideFormerPlugin.api.models.fileRelated.fileFunctions.PyFileFunctions
+import org.jetbrains.research.ideFormerPlugin.api.models.fileRelated.fileClasses.*
+import org.jetbrains.research.ideFormerPlugin.api.models.fileRelated.fileFunctions.*
 
 const val DEFAULT_DIRECTORY_NAME = "."
 const val UNSUPPORTED_FILE_EXTENSION_ERROR = "Unsupported file extension"
 
+enum class FileExtensions(val extension: String) {
+    KT("kt"),
+    JAVA("java"),
+    PY("py")
+}
+
 fun chooseFileClassesApiForFile(fileName: String, projectDirectory: PsiDirectory): FileClasses =
     when (val fileExtension = PathUtil.getFileExtension(fileName)) {
-        "kt" -> KtFileClasses(projectDirectory, fileName)
-        "java" -> JavaFileClasses(projectDirectory, fileName)
-        "py" -> PyFileClasses(projectDirectory, fileName)
+        FileExtensions.KT.extension -> KtFileClasses(projectDirectory, fileName)
+        FileExtensions.JAVA.extension -> JavaFileClasses(projectDirectory, fileName)
+        FileExtensions.PY.extension -> PyFileClasses(projectDirectory, fileName)
         else -> error("$UNSUPPORTED_FILE_EXTENSION_ERROR: $fileExtension")
     }
 
 fun chooseFileFunctionsApiForFile(fileName: String, projectDirectory: PsiDirectory): FileFunctions =
     when (val fileExtension = PathUtil.getFileExtension(fileName)) {
-        "kt" -> KtFileFunctions(projectDirectory, fileName)
-        "java" -> JavaFileFunctions(projectDirectory, fileName)
-        "py" -> PyFileFunctions(projectDirectory, fileName)
+        FileExtensions.KT.extension -> KtFileFunctions(projectDirectory, fileName)
+        FileExtensions.JAVA.extension -> JavaFileFunctions(projectDirectory, fileName)
+        FileExtensions.PY.extension -> PyFileFunctions(projectDirectory, fileName)
         else -> error("$UNSUPPORTED_FILE_EXTENSION_ERROR: $fileExtension")
     }
