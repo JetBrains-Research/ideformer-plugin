d85300560360338d6ab83f16dbf07a33a586f4cc
Evgeniia.Fedotova <ginger.ll@mail.ru>
2023-11-14 10:42:18 +0100

    add fileFileRecursively psi directory extension

--- ide-former-plugin/ide-core/src/main/kotlin/org/jetbrains/research/ideFormerPlugin/api/models/utils/PsiElementsUtils.kt
+++ ide-former-plugin/ide-core/src/main/kotlin/org/jetbrains/research/ideFormerPlugin/api/models/utils/PsiElementsUtils.kt
@@ -1,14 +1,14 @@
 package org.jetbrains.research.ideFormerPlugin.api.models.utils
 
-import com.intellij.psi.PsiDirectory
-import com.intellij.psi.PsiElement
-import com.intellij.psi.PsiFile
+import com.intellij.psi.*
 import com.intellij.psi.util.PsiTreeUtil
 
+const val PATH_DELIMITER = "/"
+
 fun PsiDirectory.findSubdirectoryRecursively(targetDirectoryPath: String): PsiDirectory? {
-    val nextDirectoryInPath = targetDirectoryPath.substringBefore("/")
+    val nextDirectoryInPath = targetDirectoryPath.substringBefore(PATH_DELIMITER)
     val currentDirectory = this.findSubdirectory(nextDirectoryInPath) ?: return null
-    val remainingDirectoryPath = targetDirectoryPath.substringAfter("/", "")
+    val remainingDirectoryPath = targetDirectoryPath.substringAfter(PATH_DELIMITER, "")
 
     return if (remainingDirectoryPath.isNotEmpty()) currentDirectory.findSubdirectoryRecursively(remainingDirectoryPath) else currentDirectory
 }
@@ -16,10 +16,25 @@ fun PsiDirectory.findSubdirectoryRecursively(targetDirectoryPath: String): PsiDi
 fun PsiDirectory.findFileByName(fileName: String): PsiFile =
     this.findFile(fileName) ?: error("No such file in the current directory")
 
+fun PsiDirectory.findFileRecursively(targetFilePath: String): PsiFile {
+    val targetFileDirectory = when (
+        val fileDirectoryPath = targetFilePath.substringBeforeLast(PATH_DELIMITER, "")
+    ) {
+        "" -> this
+        else -> this.findSubdirectoryRecursively(fileDirectoryPath) ?: error("No such file directory")
+    }
+
+    val fileName = targetFilePath.substringAfterLast(PATH_DELIMITER)
+    return targetFileDirectory.findFileByName(fileName)
+}
+
 inline fun <reified T : PsiElement> PsiFile.psiElementsOfType(): List<T> =
     PsiTreeUtil.findChildrenOfType(this, T::class.java).toList()
 
-inline fun <reified T : PsiElement> getFilePsiElementsOfType(projectDirectory: PsiDirectory, fileName: String): List<T> {
+inline fun <reified T : PsiElement> getFilePsiElementsOfType(
+    projectDirectory: PsiDirectory,
+    fileName: String
+): List<T> {
     val psiFile = projectDirectory.findFileByName(fileName)
     return psiFile.psiElementsOfType<T>()
 }
