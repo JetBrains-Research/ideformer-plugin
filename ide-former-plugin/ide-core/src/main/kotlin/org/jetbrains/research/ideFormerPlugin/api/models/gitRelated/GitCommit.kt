37d4f75a8b1518e87d42c2e50ef433b3eea6fb3a
Evgeniia.Fedotova <ginger.ll@mail.ru>
2023-11-17 15:24:54 +0100

    add git commit api

---
+++ ide-former-plugin/ide-core/src/main/kotlin/org/jetbrains/research/ideFormerPlugin/api/models/gitRelated/GitCommit.kt
@@ -0,0 +1,27 @@
+package org.jetbrains.research.ideFormerPlugin.api.models.gitRelated
+
+import com.intellij.openapi.project.Project
+import com.intellij.openapi.vfs.VirtualFile
+import git4idea.commands.*
+import org.jetbrains.research.ideFormerPlugin.api.models.ReversibleApiMethod
+
+class GitCommit(
+    private val project: Project,
+    private val projectGitRoot: VirtualFile,
+    private val commitMessage: String
+) : ReversibleApiMethod {
+
+    override fun execute() {
+        val git = Git.getInstance()
+
+        val commitCommandHandler = GitLineHandler(project, projectGitRoot, GitCommand.COMMIT)
+        commitCommandHandler.addParameters("-m", commitMessage)
+
+        val commitCommandResult: GitCommandResult = git.runCommand(commitCommandHandler)
+        if (!commitCommandResult.success()) error(commitCommandResult.errorOutputAsJoinedString)
+    }
+
+    override fun reverse() {
+        TODO("Not yet implemented")
+    }
+}
