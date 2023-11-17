74ddf34342be019be3f16f4a9bb21c7ba1c86080
Evgeniia.Fedotova <ginger.ll@mail.ru>
2023-11-17 13:48:04 +0100

    add fileName parameter to the git add server request

--- ide-former-plugin/ide-server/src/main/kotlin/org/jetbrains/research/ideFormerPlugin/server/requests/gitRelated/getGitAdd.kt
+++ ide-former-plugin/ide-server/src/main/kotlin/org/jetbrains/research/ideFormerPlugin/server/requests/gitRelated/getGitAdd.kt
@@ -3,20 +3,21 @@ package org.jetbrains.research.ideFormerPlugin.server.requests.gitRelated
 import io.ktor.server.application.*
 import io.ktor.server.routing.*
 import org.jetbrains.research.ideFormerPlugin.api.models.gitRelated.GitAdd
-import org.jetbrains.research.ideFormerPlugin.server.executeAndRespondError
-import org.jetbrains.research.ideFormerPlugin.server.respondJson
+import org.jetbrains.research.ideFormerPlugin.server.*
+import org.jetbrains.research.ideFormerPlugin.server.IdeServerConstants.FILE_NAME_REQUEST_PARAM
 import org.jetbrains.research.ideFormerPlugin.stateKeeper.IdeStateKeeper
 import org.slf4j.Logger
 
 fun Routing.getGitAdd(logger: Logger, ideStateKeeper: IdeStateKeeper) {
-    get("/git-add") {
-        // TODO: add file names list as a parameter
-        logger.info("Server GET git add is called")
+    // TODO: add the ability to send a list of file names in the request
+    get("/git-add/{$FILE_NAME_REQUEST_PARAM?}") {
+        val fileName = call.processFileNameParameter(logger) ?: return@get
+        logger.info("Server GET git add for the file '$fileName' is called")
 
-        val gitStatus = GitAdd(ideStateKeeper.userProject, ideStateKeeper.projectGitRoot, listOf("models/cat.py"))
-        if (!executeAndRespondError(gitStatus, logger)) return@get
+        val gitAdd = GitAdd(ideStateKeeper.userProject, ideStateKeeper.projectGitRoot, listOf(fileName))
+        if (!executeAndRespondError(gitAdd, logger)) return@get
 
-        call.respondJson("success")
-        logger.info("Server GET git add is processed")
+        call.respondJson("File $fileName was successfully added to Git")
+        logger.info("Server GET git add for the file '$fileName' is processed")
     }
 }
