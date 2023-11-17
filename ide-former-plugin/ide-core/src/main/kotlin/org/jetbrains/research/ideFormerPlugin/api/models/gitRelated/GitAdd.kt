a0cd4220ea1d5c7200e7612c4a0a04a8166053a8
Evgeniia.Fedotova <ginger.ll@mail.ru>
2023-11-17 13:36:07 +0100

    add git add api

---
+++ ide-former-plugin/ide-core/src/main/kotlin/org/jetbrains/research/ideFormerPlugin/api/models/gitRelated/GitAdd.kt
@@ -0,0 +1,27 @@
+package org.jetbrains.research.ideFormerPlugin.api.models.gitRelated
+
+import com.intellij.openapi.project.Project
+import com.intellij.openapi.vfs.VirtualFile
+import git4idea.commands.*
+import org.jetbrains.research.ideFormerPlugin.api.models.ReversibleApiMethod
+
+class GitAdd(
+    private val project: Project,
+    private val projectGitRoot: VirtualFile,
+    private val fileNames: List<String>
+) : ReversibleApiMethod {
+
+    override fun execute() {
+        val git = Git.getInstance()
+
+        val addCommandHandler = GitLineHandler(project, projectGitRoot, GitCommand.ADD)
+        addCommandHandler.addParameters(fileNames)
+
+        val addCommandResult: GitCommandResult = git.runCommand(addCommandHandler)
+        if (!addCommandResult.success()) error("Error while git add execution")
+    }
+
+    override fun reverse() {
+        TODO("Not yet implemented")
+    }
+}
