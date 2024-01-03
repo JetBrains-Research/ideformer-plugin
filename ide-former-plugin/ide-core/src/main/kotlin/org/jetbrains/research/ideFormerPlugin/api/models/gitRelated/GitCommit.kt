512e8348c47d157e4fca63a66ea9a7f44d07e0bb
Evgeniia.Fedotova <ginger.ll@mail.ru>
2024-01-03 20:14:14 +0100

    update git commit due to inheritance from GitApiMethod base class

--- ide-former-plugin/ide-core/src/main/kotlin/org/jetbrains/research/ideFormerPlugin/api/models/gitRelated/GitCommit.kt
+++ ide-former-plugin/ide-core/src/main/kotlin/org/jetbrains/research/ideFormerPlugin/api/models/gitRelated/GitCommit.kt
@@ -3,24 +3,16 @@ package org.jetbrains.research.ideFormerPlugin.api.models.gitRelated
 import com.intellij.openapi.project.Project
 import com.intellij.openapi.vfs.VirtualFile
 import git4idea.commands.GitCommand
-import org.jetbrains.research.ideFormerPlugin.api.models.ReversibleApiMethod
-import org.jetbrains.research.ideFormerPlugin.api.models.utils.executeGitCommand
 
 class GitCommit(
-    private val project: Project,
-    private val projectGitRoot: VirtualFile,
-    private val commitMessage: String
-) : ReversibleApiMethod {
+    project: Project,
+    projectGitRoot: VirtualFile,
+    commitMessage: String
+) : GitApiMethod(project, projectGitRoot) {
+    override var gitCommand: GitCommand? = GitCommand.COMMIT
+    override var gitCommandParameters: List<String>? = listOf("-m", commitMessage)
 
-    override fun execute() {
-        executeGitCommand(project, projectGitRoot, GitCommand.COMMIT, listOf("-m", commitMessage))
-    }
-
-    /**
-     * Invariant: reverse is always called when the head of the branch is on that particular commit
-     **/
-    override fun reverse() {
-        // drop the last commit
-        executeGitCommand(project, projectGitRoot, GitCommand.RESET, listOf("--soft", "HEAD~1"))
-    }
+    // Invariant: reverse is always called when the head of the branch is on that particular commit
+    override var gitReverseCommand: GitCommand? = GitCommand.RESET
+    override var gitReverseCommandParameters: List<String>? = listOf("--soft", "HEAD~1")
 }
