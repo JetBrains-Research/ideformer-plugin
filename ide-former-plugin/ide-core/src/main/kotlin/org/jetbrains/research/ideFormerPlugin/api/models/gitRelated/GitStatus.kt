3997896a0b3e67f9f03e7254094f1c3590cf9fb1
Evgeniia.Fedotova <ginger.ll@mail.ru>
2024-01-03 20:14:25 +0100

    update git status due to inheritance from GitApiMethod base class

--- ide-former-plugin/ide-core/src/main/kotlin/org/jetbrains/research/ideFormerPlugin/api/models/gitRelated/GitStatus.kt
+++ ide-former-plugin/ide-core/src/main/kotlin/org/jetbrains/research/ideFormerPlugin/api/models/gitRelated/GitStatus.kt
@@ -2,19 +2,21 @@ package org.jetbrains.research.ideFormerPlugin.api.models.gitRelated
 
 import com.intellij.openapi.project.Project
 import com.intellij.openapi.vfs.VirtualFile
-import git4idea.commands.*
-import org.jetbrains.research.ideFormerPlugin.api.models.IdeApiMethod
+import git4idea.commands.GitCommand
 import org.jetbrains.research.ideFormerPlugin.api.models.utils.UNCALLED_EXECUTE_BEFORE_RESULT_GETTING
 import org.jetbrains.research.ideFormerPlugin.api.models.utils.executeGitCommand
 
 class GitStatus(
     private val project: Project,
     private val projectGitRoot: VirtualFile
-) : IdeApiMethod {
+) : GitApiMethod(project, projectGitRoot) {
+    override var gitCommand: GitCommand? = GitCommand.STATUS
+    override var gitCommandParameters: List<String>? = null
+
     private var status: String? = null
 
     override fun execute() {
-        status = executeGitCommand(project, projectGitRoot, GitCommand.STATUS, null)
+        status = executeGitCommand(project, projectGitRoot, gitCommand!!, gitCommandParameters)
     }
 
     fun getStatus(): String {
