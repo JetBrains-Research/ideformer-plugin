e71b5bc06ff7db1671d88c0867a13d3760a91dc5
Evgeniia.Fedotova <ginger.ll@mail.ru>
2024-01-03 20:14:03 +0100

    update git checkout due to inheritance from GitApiMethod base class

--- ide-former-plugin/ide-core/src/main/kotlin/org/jetbrains/research/ideFormerPlugin/api/models/gitRelated/GitCheckout.kt
+++ ide-former-plugin/ide-core/src/main/kotlin/org/jetbrains/research/ideFormerPlugin/api/models/gitRelated/GitCheckout.kt
@@ -2,28 +2,24 @@ package org.jetbrains.research.ideFormerPlugin.api.models.gitRelated
 
 import com.intellij.openapi.project.Project
 import com.intellij.openapi.vfs.VirtualFile
-import git4idea.commands.*
-import org.jetbrains.research.ideFormerPlugin.api.models.ReversibleApiMethod
+import git4idea.commands.GitCommand
 import org.jetbrains.research.ideFormerPlugin.api.models.utils.executeGitCommand
 
 class GitCheckout(
-    private val project: Project,
-    private val projectGitRoot: VirtualFile,
-    private val branchName: String
-) : ReversibleApiMethod {
+    project: Project,
+    projectGitRoot: VirtualFile,
+    branchName: String
+) : GitApiMethod(project, projectGitRoot) {
     private val previousBranchName: String = executeGitCommand(
         project,
         projectGitRoot,
         GitCommand.REV_PARSE,
         listOf("--abbrev-ref", "HEAD")
-        )
+    )
 
-    override fun execute() {
-        executeGitCommand(project, projectGitRoot, GitCommand.CHECKOUT, listOf(branchName))
-    }
+    override var gitCommand: GitCommand? = GitCommand.CHECKOUT
+    override var gitCommandParameters: List<String>? = listOf(branchName)
 
-    override fun reverse() {
-        // checkout to the previous branch
-        executeGitCommand(project, projectGitRoot, GitCommand.CHECKOUT, listOf(previousBranchName))
-    }
+    override var gitReverseCommand: GitCommand? = GitCommand.CHECKOUT
+    override var gitReverseCommandParameters: List<String>? = listOf(previousBranchName)
 }
