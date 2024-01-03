fa682bd68cd9d1e2c63507a4a092d4287c8e44bc
Evgeniia.Fedotova <ginger.ll@mail.ru>
2024-01-03 20:13:47 +0100

    update git branch due to inheritance from GitApiMethod base class

--- ide-former-plugin/ide-core/src/main/kotlin/org/jetbrains/research/ideFormerPlugin/api/models/gitRelated/GitBranch.kt
+++ ide-former-plugin/ide-core/src/main/kotlin/org/jetbrains/research/ideFormerPlugin/api/models/gitRelated/GitBranch.kt
@@ -2,23 +2,16 @@ package org.jetbrains.research.ideFormerPlugin.api.models.gitRelated
 
 import com.intellij.openapi.project.Project
 import com.intellij.openapi.vfs.VirtualFile
-import git4idea.commands.*
-import org.jetbrains.research.ideFormerPlugin.api.models.ReversibleApiMethod
-import org.jetbrains.research.ideFormerPlugin.api.models.utils.executeGitCommand
+import git4idea.commands.GitCommand
 
 class GitBranch(
-    private val project: Project,
-    private val projectGitRoot: VirtualFile,
-    private val branchName: String
-) : ReversibleApiMethod {
+    project: Project,
+    projectGitRoot: VirtualFile,
+    branchName: String
+) : GitApiMethod(project, projectGitRoot) {
+    override var gitCommand: GitCommand? = GitCommand.BRANCH
+    override var gitCommandParameters: List<String>? = listOf(branchName)
 
-    override fun execute() {
-        executeGitCommand(project, projectGitRoot, GitCommand.BRANCH, listOf( branchName))
-    }
-
-    override fun reverse() {
-        // delete a new branch
-        val deleteBranchCommandParameters = listOf("--delete", branchName)
-        executeGitCommand(project, projectGitRoot, GitCommand.BRANCH, deleteBranchCommandParameters)
-    }
+    override var gitReverseCommand: GitCommand? = GitCommand.BRANCH
+    override var gitReverseCommandParameters: List<String>? = listOf("--delete", branchName)
 }
